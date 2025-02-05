2900aeeeb27c3c6837e3ff6950b045b24a3e8c5f
unknown <https://github.com/TihoH>
2025-02-05 19:04:12 +0200

    work on home

---
+++ src/components/Language.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import CustomButton from './UI/CustomButton';
+
+const Language = () => {
+    const [language, setLanguage] = useState('UA');
+
+    const { pathname } = useLocation();
+  
+    const [isAuth, setIsAuth] = useState(
+      pathname.toLocaleLowerCase() === '/login' ||
+        pathname.toLocaleLowerCase() === '/registration'
+    );
+  
+    useEffect(() => {
+      setIsAuth(pathname === '/login' || pathname === '/registration');
+    }, [pathname]);
+  
+    const setUkrainianLanguage = () => setLanguage('UA');
+    const setEnglishLanguage = () => setLanguage('EN');
+    return (
+        <div className="flex flex-1 items-end justify-end gap-[18%]">
+        <div className="flex flex-1 justify-end items-center gap-1">
+          <CustomButton
+            className={`w-[44px] h-[44px] font-raleway text-lg mobile:font-elMessiri mobile:text-sm mobile:w-7  mobile:h-7 ${language === 'UA' && 'font-semibold'} `}
+            onClick={setUkrainianLanguage}
+            buttonType="language"
+            active={language === 'UA'}
+            borderRadius={
+              language === 'UA' && 'rounded-full before:rounded-full p-1 '
+            }
+            whiteBtn={language === 'UA'}
+          >
+            УКР
+          </CustomButton>
+          <CustomButton
+            className={`w-[44px] h-[44px] font-raleway text-lg mobile:font-elMessiri mobile:text-sm mobile:w-7  mobile:h-7 ${language === 'EN' && 'font-semibold'} `}
+            onClick={setEnglishLanguage}
+            buttonType="language"
+            active={language === 'EN'}
+            borderRadius={
+              language === 'EN' && 'rounded-full before:rounded-full p-1'
+            }
+            whiteBtn={language === 'EN'}
+          >
+            EN
+          </CustomButton>
+        </div>
+      </div>
+    );
+};
+
+export default Language;
